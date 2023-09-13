a0e6f6e4c0829d78012696d63948b04d0ae37430
Tomáš Čáp <cap.tomas@outlook.com>
2023-09-13 21:55:02 +0200

    Almost working login

--- players.py
+++ players.py
@@ -24,22 +24,20 @@ def set_websocket(user_id, websocket):
     websockets[user_id] = websocket
 
 # Welcome new player.
-async def welcome(user_id, character_id, realm_id = 1):
+async def welcome(user_id, character_id):
     # Global is bad here
-    global websockets, user_states, scenario, players
+    global websockets, user_states, players
     state = {}
     user_states[user_id] = state
 
     websocket = websockets[user_id]
     character = data.get_character(character_id, True)
-    realm = db_static.get_realm()
     prompt = generate_prompt("introduction/introduce_realm")
     introduction = call_openai(prompt, 256)
     await websocket.send("NARRATION: " + introduction.replace('\n', '\n\n'))
-    exit()
-    prompt = generate_prompt("interactions/introduce_player_character", (character[0], character[2] ))
-    introduction = call_openai(prompt, 512)
-    await websocket.send("NARRATION:" + introduction.replace('\n', '\n\n'))
+    # prompt = generate_prompt("interactions/introduce_player_character", (character[0], character[2] ))
+    # introduction = call_openai(prompt, 512)
+    # await websocket.send("NARRATION:" + introduction.replace('\n', '\n\n'))
 
     await load(user_id)
 
@@ -56,9 +54,9 @@ async def load(user_id):
     websocket = websockets[user_id]
     # Load settings from previous state.
     character = data.get_user_character(user_id, True)
+    print ("here ok")
     players[character[0]] = user_id
     realm_id = character[5]
-
     realm = data.get_realm(realm_id)
     state['character'] = list(character)
     state['realm'] = list(realm)
